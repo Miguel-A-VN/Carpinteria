75b877ea24e16a3b748aa35a192eb18a9079b99d
Miguel-A-VN <mvargasnavarro19@gmail.com>
2025-09-24 23:07:17 -0500

    haciendo cambios por el ssr

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,26 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { About } from './views/about/about';
 import { Hero } from './views/hero/hero';
 import { Catalog } from './views/catalog/catalog';
 import { Contact } from './views/contact/contact';
 import { AddFurniture } from './views/add-furniture/add-furniture';
 import { Categories } from './views/categories/categories';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', component: Hero },
   { path: 'about', component: About, title: 'Sobre Nosotro' },
   { path: 'categories', component: Categories, title: 'Categorias' },
-  { path: 'category/:nameCategory', component: Catalog, title: 'Categoria' },
+  { path: 'category/:nameCategory', component: Catalog, title: 'Categoria', data: {
+      renderMode: 'ssr' // O 'dynamic' en versiones más recientes
+    } },
   { path: 'contact', component: Contact, title: 'Contacto' },
   { path: 'add', component: AddFurniture, title: 'Añadir Muebles' },
 ];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
